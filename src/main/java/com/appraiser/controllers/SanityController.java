571a1ec5a5d174ef5b1a496bba8419d74fcd5717
johnoro <officialjohnorourke@gmail.com>
2019-03-12 18:25:24 -0400

    Removes extra sanity controller and thymeleaf

--- src/main/java/com/appraiser/controllers/SanityController.java
+++ src/main/java/com/appraiser/controllers/SanityController.java
@@ -1,14 +1,22 @@
 package com.appraiser.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 @CrossOrigin
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class SanityController {
-  @RequestMapping("")
-  public String works() {
-    return "index";
+  @GetMapping("/works")
+  public List<String> works() {
+    List<String> works = new ArrayList<>();
+    works.add("It really does work!");
+    return works;
   }
 }
