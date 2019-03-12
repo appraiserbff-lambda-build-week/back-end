f75d9f5cd842260c401d132e4c08704752c82b01
johnoro <officialjohnorourke@gmail.com>
2019-03-12 11:16:42 -0400

    Adds cross origin on controllers

--- src/main/java/com/appraiser/controllers/GeneralController.java
+++ src/main/java/com/appraiser/controllers/GeneralController.java
@@ -1,6 +1,7 @@
 package com.appraiser.controllers;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class GeneralController {
   @GetMapping("/works")
