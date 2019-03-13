8b8e258f28413c508bdb84f3820bbd8ded70c0d6
johnoro <officialjohnorourke@gmail.com>
2019-03-13 18:21:45 -0400

    Moves db config out of main class and into config directory

--- src/main/java/com/appraiser/Main.java
+++ src/main/java/com/appraiser/Main.java
@@ -16,32 +16,12 @@
 
 package com.appraiser;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Main {
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
-
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
-  }
-
-  @Bean
-  public DataSource dataSource() {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    }
-
-    HikariConfig config = new HikariConfig();
-    config.setJdbcUrl(dbUrl);
-    return new HikariDataSource(config);
   }
 }
